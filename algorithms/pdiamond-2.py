eefbb79cd99e327bf1fa0ef94956d7e702f8e7c9
Riccardo De Luca <rickydeluca1@gmail.com>
2022-03-29 11:40:40 +0200

    init new version of pdiamond

---
+++ algorithms/pdiamond-2.py
@@ -0,0 +1,475 @@
+#! /usr/bin/env python
+
+
+import time
+import networkx as nx
+import numpy as np
+import copy
+import scipy.stats
+from collections import defaultdict
+import csv
+import sys
+
+
+# =============================================================================
+def print_usage():
+
+    print(' ')
+    print('        usage: python3 prob_diamond.py network_file seed_file n alpha(optional) outfile_name (optional)')
+    print('        -----------------------------------------------------------------')
+    print('        network_file : The edgelist must be provided as any delimiter-separated')
+    print('                       table. Make sure the delimiter does not exit in gene IDs')
+    print('                       and is consistent across the file.')
+    print('                       The first two columns of the table will be')
+    print('                       interpreted as an interaction gene1 <==> gene2')
+    print('        seed_file    : table containing the seed genes (if table contains')
+    print('                       more than one column they must be tab-separated;')
+    print('                       the first column will be used only)')
+    print('        n            : desired number of DIAMOnD genes, 200 is a reasonable')
+    print('                       starting point.')
+    print('        alpha        : an integer representing weight of the seeds,default')
+    print('                       value is set to 1')
+    print('        outfile_name : results will be saved under this file name')
+    print('                       by default the outfile_name is set to "first_n_added_nodes_weight_alpha.txt"')
+    print(' ')
+
+
+# =============================================================================
+def check_input_style(input_list):
+    try:
+        network_edgelist_file = input_list[1]
+        seeds_file = input_list[2]
+        max_number_of_added_nodes = int(input_list[3])
+    # if no input is given, print out a usage message and exit
+    except:
+        print_usage()
+        sys.exit(0)
+        return
+
+    alpha = 1
+    outfile_name = 'first_%d_added_nodes_weight_%d.txt' % (max_number_of_added_nodes, alpha)
+    num_iterations = 10
+
+    if len(input_list) == 5:
+        try:
+            alpha = int(input_list[4])
+            outfile_name = 'first_%d_added_weight_%d.txt' % (max_number_of_added_nodes, alpha)
+        except:
+            outfile_name = input_list[4]
+
+    if len(input_list) == 6:
+        try:
+            alpha = int(input_list[4])
+            outfile_name = input_list[5]
+        except:
+            print_usage()
+            sys.exit(0)
+            return
+    if len(input_list) == 7:
+        try:
+            alpha = int(input_list[4])
+            outfile_name = input_list[5]
+            num_iterations = input_list[6]
+        except:
+            print_usage()
+            sys.exit(0)
+            return
+
+    return network_edgelist_file, seeds_file, max_number_of_added_nodes, alpha, outfile_name, num_iterations
+
+
+# =============================================================================
+def read_input(network_file, seed_file):
+    """
+    Reads the network and the list of seed genes from external files.
+    * The edgelist must be provided as a tab-separated table. The
+    first two columns of the table will be interpreted as an
+    interaction gene1 <==> gene2
+    * The seed genes mus be provided as a table. If the table has more
+    than one column, they must be tab-separated. The first column will
+    be used only.
+    * Lines that start with '#' will be ignored in both cases
+    """
+
+    sniffer = csv.Sniffer()
+    line_delimiter = None
+    for line in open(network_file, 'r'):
+        if line[0] == '#':
+            continue
+        else:
+            dialect = sniffer.sniff(line)
+            line_delimiter = dialect.delimiter
+            break
+    if line_delimiter == None:
+        print
+        'network_file format not correct'
+        sys.exit(0)
+
+    # read the network:
+    G = nx.Graph()
+    for line in open(network_file, 'r'):
+        # lines starting with '#' will be ignored
+        if line[0] == '#':
+            continue
+        # The first two columns in the line will be interpreted as an
+        # interaction gene1 <=> gene2
+        # line_data   = line.strip().split('\t')
+        line_data = line.strip().split(line_delimiter)
+        node1 = line_data[0]
+        node2 = line_data[1]
+        G.add_edge(node1, node2)
+
+    # read the seed genes:
+    seed_genes = set()
+    for line in open(seed_file, 'r'):
+        # lines starting with '#' will be ignored
+        if line[0] == '#':
+            continue
+        # the first column in the line will be interpreted as a seed
+        # gene:
+        line_data = line.strip().split('\t')
+        seed_gene = line_data[0]
+        seed_genes.add(seed_gene)
+
+    return G, seed_genes
+
+
+# ================================================================================
+def compute_all_gamma_ln(N):
+    """
+    precomputes all logarithmic gammas
+    """
+    gamma_ln = {}
+    for i in range(1, N + 1):
+        gamma_ln[i] = scipy.special.gammaln(i)
+
+    return gamma_ln
+
+
+# =============================================================================
+def logchoose(n, k, gamma_ln):
+    if n - k + 1 <= 0:
+        return scipy.infty
+    lgn1 = gamma_ln[n + 1]
+    lgk1 = gamma_ln[k + 1]
+    lgnk1 = gamma_ln[n - k + 1]
+    return lgn1 - [lgnk1 + lgk1]
+
+
+# =============================================================================
+def gauss_hypergeom(x, r, b, n, gamma_ln):
+    return np.exp(logchoose(r, x, gamma_ln) +
+                  logchoose(b, n - x, gamma_ln) -
+                  logchoose(r + b, n, gamma_ln))
+
+
+# =============================================================================
+def pvalue(kb, k, N, s, gamma_ln):
+    """
+    -------------------------------------------------------------------
+    Computes the p-value for a node that has kb out of k links to
+    seeds, given that there's a total of s sees in a network of N nodes.
+
+    p-val = \sum_{n=kb}^{k} HypergemetricPDF(n,k,N,s)
+    -------------------------------------------------------------------
+    """
+    p = 0.0
+    for n in range(kb, k + 1):
+        if n > s:
+            break
+        prob = gauss_hypergeom(n, s, N - s, k, gamma_ln)
+        # print prob
+        p += prob
+
+    if p > 1:
+        return [1]
+    else:
+        return p
+
+    # =============================================================================
+
+
+def get_neighbors_and_degrees(G):
+    neighbors, all_degrees = {}, {}
+    for node in G.nodes():
+        nn = set(G.neighbors(node))
+        neighbors[node] = nn
+        all_degrees[node] = G.degree(node)
+
+    return neighbors, all_degrees
+
+
+# =============================================================================
+# Reduce number of calculations
+# =============================================================================
+def reduce_not_in_cluster_nodes(all_degrees, neighbors, G, not_in_cluster, cluster_nodes, alpha):
+    reduced_not_in_cluster = {}
+    kb2k = defaultdict(dict)
+    for node in not_in_cluster:
+
+        k = all_degrees[node]
+        kb = 0
+        # Going through all neighbors and counting the number of module neighbors
+        for neighbor in neighbors[node]:
+            if neighbor in cluster_nodes:
+                kb += 1
+
+        # adding wights to the the edges connected to seeds
+        k += (alpha - 1) * kb
+        kb += (alpha - 1) * kb
+        kb2k[kb][k] = node
+
+    # Going to choose the node with largest kb, given k
+    k2kb = defaultdict(dict)
+    for kb, k2node in kb2k.items():
+        min_k = min(k2node.keys())
+        node = k2node[min_k]
+        k2kb[min_k][kb] = node
+
+    for k, kb2node in k2kb.items():
+        max_kb = max(kb2node.keys())
+        node = kb2node[max_kb]
+        reduced_not_in_cluster[node] = (max_kb, k)
+
+    return reduced_not_in_cluster
+
+
+# =============================================================================
+# Transform a list in a probabilistic array
+# =============================================================================
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+# ======================================================================================
+#   C O R E    A L G O R I T H M
+# ======================================================================================
+def pdiamond_iteration_of_first_X_nodes(G, S, X, alpha):
+    """
+    Parameters:
+    ----------
+    - G:     graph
+    - S:     seeds
+    - X:     the number of iterations, i.e only the first X gened will be
+             pulled in
+    - alpha: seeds weight
+    Returns:
+    --------
+
+    - added_nodes: ordered list of nodes in the order by which they
+      are agglomerated. Each entry has 4 info:
+      * name : dito
+      * k    : degree of the node
+      * kb   : number of +1 neighbors
+      * p    : p-value at agglomeration
+    """
+
+    N = G.number_of_nodes()
+
+    added_nodes = []
+
+    # ------------------------------------------------------------------
+    # Setting up dictionaries with all neighbor lists
+    # and all degrees
+    # ------------------------------------------------------------------
+    neighbors, all_degrees = get_neighbors_and_degrees(G)
+
+    # ------------------------------------------------------------------
+    # Setting up initial set of nodes in cluster
+    # ------------------------------------------------------------------
+
+    cluster_nodes = set(S)
+    not_in_cluster = set()
+    s0 = len(cluster_nodes)
+
+    s0 += (alpha - 1) * s0
+    N += (alpha - 1) * s0
+
+    # ------------------------------------------------------------------
+    # precompute the logarithmic gamma functions
+    # ------------------------------------------------------------------
+    gamma_ln = compute_all_gamma_ln(N + 1)
+
+    # ------------------------------------------------------------------
+    # Setting initial set of nodes not in cluster
+    # ------------------------------------------------------------------
+    for node in cluster_nodes:
+        not_in_cluster |= neighbors[node]
+    not_in_cluster -= cluster_nodes
+
+    # ------------------------------------------------------------------
+    #
+    # M A I N     L O O P
+    #
+    # ------------------------------------------------------------------
+
+    all_p = {}
+    SNP = 0
+    while len(added_nodes) < X:
+
+        # ------------------------------------------------------------------
+        #
+        # Going through all nodes that are not in the cluster yet and
+        # record k, kb and p
+        #
+        # ------------------------------------------------------------------
+
+        info = {}
+
+        pmin = 10
+        next_node = 'nix'
+        reduced_not_in_cluster = reduce_not_in_cluster_nodes(all_degrees,
+                                                             neighbors, G,
+                                                             not_in_cluster,
+                                                             cluster_nodes, alpha)
+        probable_next_nodes = []
+        inv_p_values = []
+        SNP_iter = 0
+        for node, kbk in reduced_not_in_cluster.items():
+            # Getting the p-value of this kb,k
+            # combination and save it in all_p, so computing it only once!
+            kb, k = kbk
+            try:
+                p = all_p[(k, kb, s0)]
+            except KeyError:
+                p = pvalue(kb, k, N, s0, gamma_ln)
+                all_p[(k, kb, s0)] = p
+
+            '''
+            # recording the node with smallest p-value
+            if p < pmin:
+                pmin = p
+                next_node = node
+            '''
+
+            # Compute the
+
+            info[node] = (k, kb, p)
+            probable_next_nodes.append(node)
+            inv_p_values.append(1 - p[0])
+            SNP_iter += p
+
+        averaged_SNP_iter = SNP_iter / len(reduced_not_in_cluster.items())
+
+        # print(probable_next_nodes)
+
+        # ---------------------------------------------------------------------
+        # Convert the p-value list in a probability distribution and
+        # extract the next node based on it
+        # ---------------------------------------------------------------------
+        probability_distribution = softmax(inv_p_values)
+        next_node = np.random.choice(probable_next_nodes, 1,
+                                     p=probability_distribution)
+        next_node = next_node[0]
+        # print(next_node)
+
+        # ---------------------------------------------------------------------
+        # Adding the chosen node to the list of agglomerated nodes
+        # ---------------------------------------------------------------------
+        added_nodes.append((next_node,
+                            info[next_node][0],
+                            info[next_node][1],
+                            info[next_node][2]))
+
+        # Updating the list of cluster nodes and s0
+        cluster_nodes.add(next_node)
+        s0 = len(cluster_nodes)
+        not_in_cluster |= (neighbors[next_node] - cluster_nodes)
+        not_in_cluster.remove(next_node)
+
+        SNP += averaged_SNP_iter
+
+    return added_nodes, SNP
+
+
+# ===========================================================================
+#
+#   M A I N    P R O B   D I A M O n D    A L G O R I T H M
+#
+# ===========================================================================
+def pDIAMOnD(G_original, seed_genes, max_number_of_added_nodes, alpha, outfile=None, max_iterations=10):
+
+    # 1. throwing away the seed genes that are not in the network
+    all_genes_in_network = set(G_original.nodes())
+    seed_genes = set(seed_genes)
+    disease_genes = seed_genes & all_genes_in_network
+
+    if len(disease_genes) != len(seed_genes):
+        print("PROB DIAMOnD(): ignoring %s of %s seed genes that are not in the network" % (
+            len(seed_genes - all_genes_in_network), len(seed_genes)))
+
+    # 2. run pdiamond different times and get the best
+    #    predicted nodes combination wrt the sum of the
+    #    p-values of the neighbours (SNP).
+    print(f"pDIAMOnD(): number of iterations = {max_iterations}")
+    lowest_SNP = 100
+    best_added_nodes = []
+
+    for i in range(max_iterations):
+        added_nodes, SNP = pdiamond_iteration_of_first_X_nodes(G_original,
+                                                              disease_genes,
+                                                              max_number_of_added_nodes, alpha)
+        if SNP > 0 and SNP < lowest_SNP:
+            lowest_SNP = SNP
+            best_added_nodes = added_nodes
+
+    return best_added_nodes
+
+def run_prob_diamond(input_list):
+    network_edgelist_file, seeds_file, max_number_of_added_nodes, alpha, outfile_name, num_iterations = check_input_style(input_list)
+
+    # read the network and the seed genes:
+    G_original, seed_genes = read_input(network_edgelist_file, seeds_file)
+
+    # run DIAMOnD
+    added_nodes = pDIAMOnD(G_original,
+                               seed_genes,
+                               max_number_of_added_nodes, alpha,
+                               outfile=outfile_name,
+                               max_iterations=num_iterations)
+
+    print("\n results have been saved to '%s' \n" % outfile_name)
+
+    return added_nodes
+
+
+# ===========================================================================
+#
+# "Hey Ho, Let's go!" -- The Ramones (1976)
+#
+# ===========================================================================
+
+
+if __name__ == '__main__':
+    # -----------------------------------------------------
+    # Checking for input from the command line:
+    # -----------------------------------------------------
+    #
+    # [1] file providing the network in the form of an edgelist
+    #     (tab-separated table, columns 1 & 2 will be used)
+    #
+    # [2] file with the seed genes (if table contains more than one
+    #     column they must be tab-separated; the first column will be
+    #     used only)
+    #
+    # [3] number of desired iterations
+    #
+    # [4] (optional) seeds weight (integer), default value is 1
+    # [5] (optional) name for the results file
+
+    # check if input style is correct
+    input_list = sys.argv
+    network_edgelist_file, seeds_file, max_number_of_added_nodes, alpha, outfile_name, num_iterations = check_input_style(input_list)
+
+    # read the network and the seed genes:
+    G_original, seed_genes = read_input(network_edgelist_file, seeds_file)
+
+
+    # run Prob DIAMOnD
+    added_nodes = pDIAMOnD(G_original,
+                               seed_genes,
+                               max_number_of_added_nodes, alpha,
+                               outfile=outfile_name,
+                               max_iterations=num_iterations)
+
+    print("\n results have been saved to '%s' \n" % outfile_name)
