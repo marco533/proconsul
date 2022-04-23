74c7824d0923dc47cd37aa49462db11632d1e54d
Riccardo De Luca <rickydeluca1@gmail.com>
2022-04-23 18:06:08 +0200

    update tables

--- disease_analysis.py
+++ disease_analysis.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from algorithms.diamond import DIAMOnD
 from algorithms.pdiamond import pDIAMOnD, pDIAMOnD_alternative
+from utils.data_utils import string_to_filename
 from utils.network_utils import *
 
 # =======================
@@ -75,93 +76,12 @@ def read_terminal_input(args):
 # =======================================
 #   N E T W O R K   C O M P A R I S O N
 # =======================================
-def compare_networks(diseases, hhi_df, hhi, LCC_hhi):
+def compare_disease_networks(disease_networks, enriching_algorithm=None):
 
     # Define attributes
     attributes = ["number_of_disease_genes",
                   "number_of_connected_components",
-                  "LCC_size",
-                  "density",
-                  "percentage_of_disease_genes_in_LCC",
-                  "longest_path_length_in_LCC",
-                  "longest_path_lenght_in_interactome",
-                  "average_path_length",
-                  "degree_distribution",
-                  "average_degree",
-                  "clustering_coefficient",
-                  "modularity",
-                  "global_efficency",
-                  "assortativity"]
-
-    # Init disease attributes dictionary
-    disease_attributes_dictionary = {}
-
-    # Fill the dictionary
-    for disease in diseases:
-        disease_attributes_dictionary[disease] = []
-
-        # 1. number_of_disease_genes
-        disease_genes = get_disease_genes_from_gda("data/curated_gene_disease_associations.tsv", disease, training_mode=True)
-        disease_attributes_dictionary[disease].append(len(disease_genes))
-
-        # 2. number_of_connected_components
-        num_connected_components = get_disease_num_connected_components(hhi_df, disease, from_curated=True)
-        disease_attributes_dictionary[disease].append(num_connected_components)
-
-        # 3. LCC_size
-        disease_LCC = get_disease_LCC(hhi_df, disease, from_curated=True)
-        disease_LCC = LCC_hhi.subgraph(disease_LCC).copy()
-
-        disease_attributes_dictionary[disease].append(nx.number_of_nodes(disease_LCC))
-
-        # 4. density
-        disease_density = get_density(disease_LCC)
-        disease_attributes_dictionary[disease].append(disease_density)
-
-        # 5. percentage_of_disease_genes_in_LCC
-        disease_attributes_dictionary[disease].append(get_genes_percentage(disease_genes, disease_LCC))
-
-        # 6. longest_path_in_LCC
-        disease_attributes_dictionary[disease].append(get_longest_path_for_a_disease_LCC(disease))
-
-        # 7. longest_path_in_interactome
-        disease_attributes_dictionary[disease].append(get_longest_path_for_a_disease_interactome(disease))
-
-        # TODO: continue following the attributes order
-
-        # 8. average_path_length
-        disease_attributes_dictionary[disease].append("test")
-
-        # 9. degree_distribution
-        disease_attributes_dictionary[disease].append("test")
-
-        # 10. average_degree
-        disease_attributes_dictionary[disease].append("test")
-
-        # 11. clustering_coefficient
-        disease_attributes_dictionary[disease].append("test")
-
-        # 12. modularity
-        disease_attributes_dictionary[disease].append("test")
-
-        # 13. global_efficency
-        disease_attributes_dictionary[disease].append("test")
-
-        # 14. assortativity
-        disease_attributes_dictionary[disease].append("test")
-
-    # Save the dictionary as CSV fils
-    df = pd.DataFrame.from_dict(disease_attributes_dictionary, orient='index',
-                      columns=attributes)
-
-    df.to_csv("tables/top_performing_diseases_analysis.csv")
-
-
-def compare_enriched_networks(diseases, hhi_df, hhi, LCC_hhi, enriched_genes=None, enrichement_algorithm=None):
-
-    # Define attributes
-    attributes = ["number_of_disease_genes",
-                  "number_of_connected_components",
+                  "percentage_of_connected_components",
                   "LCC_size",
                   "density",
                   "density_of_LCC",
@@ -180,11 +100,12 @@ def compare_enriched_networks(diseases, hhi_df, hhi, LCC_hhi, enriched_genes=Non
     disease_attributes_dictionary = {}
 
     # Fill the dictionary
-    for idx, disease in enumerate(diseases):
-        disease_attributes_dictionary[disease] = []
+    i = 0
+    for disease, disease_network in disease_networks.items():
+        i += 1
+        print(f"disease_network {i} of {len(disease_networks)}: {disease}")
 
-        # Build the disease network
-        disease_network = LCC_hhi.subgraph(enriched_genes[idx]).copy()
+        disease_attributes_dictionary[disease] = []
 
         # 1. number_of_disease_genes
         number_of_disease_genes = disease_network.number_of_nodes()
@@ -195,58 +116,75 @@ def compare_enriched_networks(diseases, hhi_df, hhi, LCC_hhi, enriched_genes=Non
         number_of_connected_components = len(connected_components)
         disease_attributes_dictionary[disease].append(number_of_connected_components)
 
-        # 3. LCC_size
+        # 3. percentage_of_connected_components
+        percentage_connected_components = number_of_connected_components / number_of_disease_genes
+        disease_attributes_dictionary[disease].append(percentage_connected_components)
+
+        # 4. LCC_size
         disease_LCC = max(connected_components, key=len)
         disease_LCC = LCC_hhi.subgraph(disease_LCC).copy()
         disease_LCC_size = nx.number_of_nodes(disease_LCC)
 
         disease_attributes_dictionary[disease].append(disease_LCC_size)
 
-        # 4. density
+        # 5. density
         density = nx.density(disease_network)
         disease_attributes_dictionary[disease].append(density)
 
-        # 5. density_of_LCC
+        # 6. density_of_LCC
         density_of_LCC = nx.density(disease_LCC)
         disease_attributes_dictionary[disease].append(density_of_LCC)
 
-        # 6. percentage_of_disease_genes_in_LCC
+        # 7. percentage_of_disease_genes_in_LCC
         disease_attributes_dictionary[disease].append(disease_LCC_size / number_of_disease_genes)
 
-        # 7. longest_path_in_LCC
+        # 8. longest_path_in_LCC
+        # disease_attributes_dictionary[disease].append(get_longest_path_for_a_disease_LCC(disease))
         disease_attributes_dictionary[disease].append("test")
 
-        # 8. longest_path_in_interactome
+        # 9. longest_path_in_interactome
+        # disease_attributes_dictionary[disease].append(get_longest_path_for_a_disease_interactome(disease))
         disease_attributes_dictionary[disease].append("test")
 
         # TODO: continue following the attributes order
 
-        # 9. average_path_length
+        # 10. average_path_length
         disease_attributes_dictionary[disease].append("test")
 
-        # 10. degree_distribution
+        # 11. degree_distribution
         disease_attributes_dictionary[disease].append("test")
 
-        # 11. average_degree
+        # 12. average_degree
         disease_attributes_dictionary[disease].append("test")
 
-        # 12. clustering_coefficient
+        # 13. clustering_coefficient
         disease_attributes_dictionary[disease].append("test")
 
-        # 13. modularity
+        # 14. modularity
         disease_attributes_dictionary[disease].append("test")
 
-        # 14. global_efficency
+        # 15. global_efficency
         disease_attributes_dictionary[disease].append("test")
 
-        # 15. assortativity
+        # 16. assortativity
         disease_attributes_dictionary[disease].append("test")
+
+        # Export the network for Cytoscape visualization
+        if enriching_algorithm is not None:
+            nx.write_graphml(disease_network, f"plots/networks/graphml/{string_to_filename(disease)}.{enriching_algorithm}_enriched.graphml")
+        else:
+            nx.write_graphml(disease_network, f"plots/networks/graphml/{string_to_filename(disease)}.graphml")
+
 
     # Save the dictionary as CSV fils
     df = pd.DataFrame.from_dict(disease_attributes_dictionary, orient='index',
                       columns=attributes)
 
-    df.to_csv(f"tables/top_performing_diseases_analysis.{enrichement_algorithm}_enriched.csv")
+    if enriching_algorithm is not None:
+        df.to_csv(f"tables/top_performing_diseases_analysis.{enriching_algorithm}_enriched.csv")
+    else:
+        df.to_csv("tables/top_performing_diseases_analysis.csv")
+
 
 # ============
 #   M A I N
@@ -273,13 +211,37 @@ if __name__ == "__main__":
     LCC_hhi = isolate_LCC(hhi)
     LCC_hhi = hhi.subgraph(LCC_hhi).copy()
 
-    # Compare disease networks BEFORE enrichment
-    # compare_networks(diseases, hhi_df, hhi, LCC_hhi)
+    # ******************************
+    #   Build the Disease Networks
+    # ******************************
 
-    # Perform enrichment through DIAMOnD and pDIAMOnD
-    DIAMOnD_enriched_genes  = []     # List of list of disease genes:
-                                     # each sublist contains the enriched genes of a disease
-    pDIAMOnD_enriched_genes = []
+    # Dictionary of networks
+    disease_networks = {}
+
+    # Build the network for each disease
+    # and append it to the list
+    for disease in diseases:
+        # Get the disease genes, i.e. the nodes from
+        # which we can extract the subgraph from the original interactome
+        disease_genes = get_disease_genes_from_gda("data/curated_gene_disease_associations.tsv", disease, training_mode=True)
+
+        # Extract the subgraph from the HHI LCC and remove the self-loops
+        disease_network = LCC_hhi.subgraph(disease_genes).copy()
+        disease_network.remove_edges_from(nx.selfloop_edges(disease_network))
+
+        # Add it to the dictionary of networks
+        disease_networks[disease] = disease_network
+
+    # print("List of Disease Networks: ", disease_networks)
+
+    compare_disease_networks(disease_networks)
+
+    # ***************************************************
+    #   Perform enrichment through DIAMOnD and pDIAMOnD
+    # ***************************************************
+
+    DIAMOnD_enriched_networks   = {}    # Dict of disease networks enriched with DIAMOnD
+    pDIAMOnD_enriched_networks  = {}    # Dict of disease networks enriched with pDIAMOnD
 
     for disease in diseases:
         disease_genes = get_disease_genes_from_gda("data/curated_gene_disease_associations.tsv", disease, training_mode=True)
@@ -289,24 +251,37 @@ if __name__ == "__main__":
 
         added_nodes = DIAMOnD(LCC_hhi, disease_genes, 25, 1, outfile="tmp/diamond_output.txt")
         predicted_genes = [item[0] for item in added_nodes]
-        DIAMOnD_enriched_genes.append(disease_genes + predicted_genes)
+        DIAMOnD_enriched_genes = disease_genes + predicted_genes
 
-        print(f"orginal_list: {len(disease_genes)} elements")
-        print(f"predicted_list: {len(predicted_genes)} elements")
-        print(f"enriched_list: {len(DIAMOnD_enriched_genes)} elements")
+        print(f"num_orginal_genes: {len(disease_genes)} genes")
+        print(f"num_predicted_genes: {len(predicted_genes)} genes")
+        print(f"total_enriched_genes: {len(DIAMOnD_enriched_genes)} genes")
 
+        # Extract the subgraph from the HHI LCC and remove the self-loops
+        disease_network = LCC_hhi.subgraph(DIAMOnD_enriched_genes).copy()
+        disease_network.remove_edges_from(nx.selfloop_edges(disease_network))
+
+        # Add it to the dictionary of DIAMOnD enriched networks
+        DIAMOnD_enriched_networks[disease] = disease_network
 
         # pDIAMOnd
         print(f"\nPerforming the enrichemt of {disease} with pDIAMOnD\n")
 
         added_nodes = pDIAMOnD(LCC_hhi, disease_genes, 25, 1, outfile="tmp/pdiamond_output.txt")
         predicted_genes = [item[0] for item in added_nodes]
-        pDIAMOnD_enriched_genes.append(disease_genes + predicted_genes)
+        pDIAMOnD_enriched_genes = disease_genes + predicted_genes
 
-        print(f"orginal_list: {len(disease_genes)} elements")
-        print(f"predicted_list: {len(predicted_genes)} elements")
-        print(f"enriched_list: {len(pDIAMOnD_enriched_genes)} elements")
+        print(f"num_orginal_genes: {len(disease_genes)} genes")
+        print(f"num_predicted_genes: {len(predicted_genes)} genes")
+        print(f"total_enriched_genes: {len(pDIAMOnD_enriched_genes)} genes")
+
+        # Extract the subgraph from the HHI LCC and remove the self-loops
+        disease_network = LCC_hhi.subgraph(pDIAMOnD_enriched_genes).copy()
+        disease_network.remove_edges_from(nx.selfloop_edges(disease_network))
+
+        # Add it to the dictionary of pDIAMOnD enriched networks
+        pDIAMOnD_enriched_networks[disease] = disease_network
 
     # Compare disease networks AFTER the enrichment
-    compare_enriched_networks(diseases, hhi_df, hhi, LCC_hhi, enriched_genes=DIAMOnD_enriched_genes, enrichement_algorithm="DIAMOnD")
-    compare_enriched_networks(diseases, hhi_df, hhi, LCC_hhi, enriched_genes=pDIAMOnD_enriched_genes, enrichement_algorithm="pDIAMOnD")
+    compare_disease_networks(DIAMOnD_enriched_networks, enriching_algorithm="DIAMOnD")
+    compare_disease_networks(pDIAMOnD_enriched_networks, enriching_algorithm="pDIAMOnD")
